5b9ba5a5e0599c236562e993fc03b609d36eefcd
Báder László <bader.laszlo22@gmail.com>
2016-05-30 14:12:47 +0200

    Half way

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 $(function(){
-    
+
+    $('.sizes > div').on('click', function (e) {
+
+    	 e.preventDefault();
+
+    	 $('.sizes > div').removeClass('active');
+
+    	 $(this).addClass('active');
+    })
+
+    $('.tabs a').on('click', function  (e) {
+
+    	e.preventDefault();
+    	
+    	$('.tabs a').removeClass('active');
+
+    	$(this).addClass('active');
+
+    })
+
 });
