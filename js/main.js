ac24d0ad10a2e2574f08fd09d06658bab04c17b5
Báder László <bader.laszlo22@gmail.com>
2016-05-30 16:15:36 +0200

    Last of day commit bro

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ $(function(){
     	 $('.sizes > div').removeClass('active');
 
     	 $(this).addClass('active');
-    })
+    });
 
     $('.tabs a').on('click', function  (e) {
 
@@ -16,6 +16,42 @@ $(function(){
     	$('.tabs a').removeClass('active');
 
     	$(this).addClass('active');
+
+    });
+
+    $('.tab-nav a').on('click', function  (e) {
+
+        e.preventDefault();
+         
+        $('.tab-nav a').removeClass('active');
+
+        $(this).addClass('active');
+
+        var id = $(this).data('id');
+
+        $('.boxes > div').hide();
+
+        $('.boxes').find('.' + id).show();
+         
+    });
+
+    $('.switcher a').on('click', function  (e) {
+
+        e.preventDefault();
+
+        $('.switcher a').removeClass('active');
+
+        $(this).addClass('active');
+        
+    });
+
+    $('.colors a').on('click', function  (e) {
+
+        e.preventDefault();
+        
+        $('.colors a').removeClass('active');
+
+        $(this).addClass('active');
 
     })
 
